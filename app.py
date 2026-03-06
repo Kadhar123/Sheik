f6a87510c29d8d355fabf8a8e796afd5034c63c5
ankurrathour13 <ankurrathour@yahoo.com>
2026-03-06 15:27:44 +0530

    replaced the old code with basic hello word
    replaced the old

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-#from flask import Flask
+# app.py
 
-# Create a Flask app
-app = Flask(__name__)
-
-# Define a route for the homepage
-@app.route("/")
-def hello():
-    return "<h1>Hello Ankur</h1>"
-
-# Run the app
-if __name__ == "__main__":
-
-    app.run(debug=True)
-    # 4th comment line added 
-# new line added from personal laptop to be pushed back to github
-
+print("Hello World")
